378381fd520ddaf405092c13ffc7baed68c91c04
plop <plop.dev0@gmail.com>
2024-07-19 20:38:37 +0100

    finished order page, added new packages

--- src/scripts/email.js
+++ src/scripts/email.js
@@ -1,4 +1,6 @@
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder } from 'discord.js';
 import client from './bot';
+import url from 'url';
 import { ImapFlow } from 'imapflow';
 import * as schedule from 'node-schedule';
 const { EMAIL_APP_PASSWORD } = import.meta.env;
@@ -24,31 +26,40 @@ const main = async () => {
 	let lock = await Imapclient.getMailboxLock('INBOX'); // Lock and select the 'INBOX' mailbox.
 	try {
 		const emailList = [];
-		for await (let message of Imapclient.fetch('1:*', { envelope: true })) {
+		for await (let message of Imapclient.fetch('1:5', { envelope: true })) {
+			const gmailLink = `https://mail.google.com/mail/u/0/#inbox`;
+
 			emailList.push({
 				uid: message.uid,
 				subject: message.envelope.subject,
 				from: message.envelope.from[0].name || message.envelope.from[0].address,
 				date: message.envelope.date,
+				size: message.size,
+				link: gmailLink,
 			});
 		}
 
-		// Log the email information in a clean format
-		let message = '---\n';
-		emailList.forEach(email => {
-			message += `UID: ${email.uid}\n`;
-			message += `Subject: ${email.subject}\n`;
-			message += `From: ${email.from}\n`;
-			message += `Date: ${email.date}\n`;
-			message += '---\n';
-		});
-
-		const emailChannelId = '1166426519845994607'; // testing channel for now
+		const emailChannelId = '1164312818770788513'; // testing channel for now
 		try {
 			const channel = await client.channels.fetch(emailChannelId).then(res => res);
 			if (channel) {
-				if (message !== '---\n') {
-					await channel.send({ content: `Unread email(s):\n\`\`\`\n${message}\`\`\`` });
+				if (emailList.length >= 0) {
+					await channel.send({ content: `First 5 unread emails:` });
+					emailList.forEach(async email => {
+						const embed = new EmbedBuilder()
+							.setColor([60, 40, 230])
+							.setTimestamp()
+							.setTitle(`${email.from}: ${email.subject}`)
+							.setDescription(`UID: ${email.uid}\nDate Sent: ${email.date}`);
+
+						const readButton = new ButtonBuilder().setCustomId(`read-${email.uid}`).setLabel('Mark as read').setStyle(ButtonStyle.Primary);
+						const deleteButton = new ButtonBuilder().setCustomId(`delete-${email.uid}`).setLabel('Delete').setStyle(ButtonStyle.Danger);
+						const linkButton = new ButtonBuilder().setLabel('Open inbox in browser').setStyle(ButtonStyle.Link).setURL(email.link);
+
+						const row = new ActionRowBuilder().addComponents(readButton, deleteButton, linkButton);
+
+						await channel.send({ embeds: [embed], components: [row] });
+					});
 				}
 			}
 		} catch (error) {
@@ -61,6 +72,30 @@ const main = async () => {
 	await Imapclient.logout(); // Log out and close the connection.
 };
 
-schedule.scheduleJob('15 * * * *', () => {
-	main();
+// schedule.scheduleJob('10 * * * * *', () => {
+// 	main();
+// });
+
+client.on('interactionCreate', async interaction => {
+	if (interaction.isButton()) {
+		const emailUID = interaction.customId.split('-')[1];
+		const customId = interaction.customId.split('-')[0];
+		if (customId === 'read') {
+			try {
+				await Imapclient.messageFlagsAdd('1:*', ['Seen'], { uid: emailUID });
+				await interaction.reply({ content: `Email successfully marked as read.`, ephemeral: true });
+			} catch (error) {
+				console.log('An error occured: ' + error.message + '\n' + error);
+				await interaction.reply({ content: `An error occured. Contact \`plop_dev\` for assistance or try again later.`, ephemeral: true });
+			}
+		} else if (customId === 'delete') {
+			try {
+				await Imapclient.messageDelete('1:*', { uid: emailUID });
+				await interaction.reply({ content: `Email successfully deleted.`, ephemeral: true });
+			} catch (error) {
+				console.log('An error occured: ' + error.message + '\n' + error);
+				await interaction.reply({ content: `An error occured. Contact \`plop_dev\` for assistance or try again later.`, ephemeral: true });
+			}
+		}
+	}
 });
