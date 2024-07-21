a14b3cf00bea06ab57a57b9eae9b9d33e8d1f34a
plop <plop.dev0@gmail.com>
2024-07-21 20:23:14 +0100

    updated colours, made sidebar look a little better, updated images

--- src/tools/availableColours.ts
+++ src/tools/availableColours.ts
@@ -7,9 +7,9 @@ interface Colour {
 }
 
 const defExp: Colour[] = [
-	{ name: 'White', hex: '#fff', blackBorder: true, default: true },
+	{ name: 'White', hex: '#fff', blackBorder: true, unavailable: true },
 	{ name: 'Red', hex: '#be3b3b', unavailable: true },
-	{ name: 'Black', hex: '#000' },
+	{ name: 'Black', hex: '#000', default: true },
 	{ name: 'Light-Green', hex: '#8cc379', blackBorder: true },
 	{ name: 'Cloudy', hex: '#949DA0', blackBorder: true },
 	{ name: 'Blue', hex: '#2182b3', unavailable: true },
